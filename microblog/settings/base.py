9976c49e6d1d23bf65346fa5100aac3ad5490353
whenther <william.lee.wagner@gmail.com>
2013-09-09 05:00:39 +0000

    foo

--- microblog/settings/base.py
+++ microblog/settings/base.py
@@ -74,7 +74,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = root('..', 'uploads')
+MEDIA_ROOT = '/home/whenther/public_html/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
